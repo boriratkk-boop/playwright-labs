555686491a89facbab1f9711697f477d400b29f9
Borirat <Boriratkk@gmail.com>
2026-03-07 03:39:10 +0700

    update product title 24

--- automation/playwright.config.ts
+++ automation/playwright.config.ts
@@ -87,7 +87,7 @@ export default defineConfig({
   }
 ]
 
-  /* Run your local dev server before starting the tests */
+  //* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://localhost:3000',
