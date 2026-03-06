3796c383f2cb34bed3dbc9a44cb743b80a4a1205
Borirat <Boriratkk@gmail.com>
2026-03-07 03:48:55 +0700

    update product title 25

--- automation/playwright.config.ts
+++ automation/playwright.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  //* Retry on CI only */
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  //* Opt out of parallel tests on CI. */
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  //* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
@@ -87,10 +87,10 @@ export default defineConfig({
   }
 ]
 
-  //* Run your local dev server before starting the tests */
+  /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
-  // },
+  /// },
 });
