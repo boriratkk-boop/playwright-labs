98ce27fd371cda4fcca062a912b969c8c950e4fa
Borirat <Boriratkk@gmail.com>
2026-03-07 14:36:48 +0700

    update product title 47

--- automation/tests/smoke/product.spec.ts
+++ automation/tests/smoke/product.spec.ts
@@ -4,7 +4,7 @@ test('product list should display', async ({ page }) => {
 
   await page.goto("http://localhost:3000")
 
-  const products = page.locator('[data-testid="product-item"]')
+  const products = page.locator('[data-testid="product-items"]')
 
   await expect(products).toHaveCount(3)
 
